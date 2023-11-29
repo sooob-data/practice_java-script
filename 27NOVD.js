026a26de3784df3ed7abca3175ad51ae7b4573dc
Shubham yadav <149668175+sooob-data@users.noreply.github.com>
2023-11-29 10:52:39 +0000

    E left

---
+++ 27NOVD.js
@@ -0,0 +1,11 @@
+//creat an array of five planets and another array containing three satellites.(proper casing)
+// now in new array named galaxy ,combine these two together with each words lowercased?
+
+const planets=["Moon","Jupiter","Saturn","Neptune"]
+const satellites = ["Europa","Titan","Miranda"]
+const galaxy = []
+const lowercasedPlanets = toLowerCase(planets);
+const lowercasedSatellites = toLowerCase(satellites);
+galaxy.push(lowercasedPlanet);
+galaxy.push(lowercasedsatellites);
+console.log(galaxy.flat());
